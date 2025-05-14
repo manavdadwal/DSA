ff93f6a61e5184109787bd5ff38abf27c9f62245
Manav Dadwal <manav.dadwal@ril.com>
2025-05-14 11:36:32 +0530

    Added Plus Minus Function.

---
+++ Problems/1_Easy/5_Plus Minus/plus_minus.js
@@ -0,0 +1,37 @@
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    let len = arr.length;
+
+    /**
+     * @param
+     * This below params will store the count of the numbers in the array.
+     * @positiveInt
+     * @negativeInt
+     * @zeroInt
+     */
+
+    let positiveInt = 0;
+    let negativeInt = 0;
+    let zeroInt = 0;
+    for (let i = 0; i < len; i++) {
+        if (arr[i] > 0) {
+            positiveInt++;
+        }
+        if (arr[i] < 0) {
+            negativeInt++;
+        }
+        if (arr[i] == 0) {
+            zeroInt++;
+        }
+    }
+    console.log(positiveInt / len)
+    console.log(Math.abs(negativeInt) / len)
+    console.log(zeroInt / len)
+    return [positiveInt / len, Math.abs(negativeInt) / len, zeroInt / len];
+}
---
+++ Problems/1_Easy/5_Plus Minus/readme.md
@@ -0,0 +1,4 @@
+Problem Statement
+
+![alt text](/assets/1_Easy/5_Plus%20Minus/image.png)
+![alt text](/assets/1_Easy/5_Plus%20Minus/image-1.png)
